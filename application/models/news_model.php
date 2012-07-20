a9605364196b501b75cee2183c5d9ebf076dfe56
Andrew Prasetya <bkdevstudio@gmail.com>
2012-07-20 10:59:57 -0400

    configed databases, fixed news model bug

--- application/models/news_model.php
+++ application/models/news_model.php
@@ -1,13 +1,21 @@
 <?php
-public function get_news($slug = FALSE)
-{
-	if ($slug === FALSE)
+class News_model extends CI_Model {
+
+	public function __construct()
 	{
-		$query = $this->db->get('news');
-		return $query->result_array();
+		$this->load->database();
 	}
-	
-	$query = $this->db->get_where('news', array('slug' => $slug));
-	return $query->row_array();
+
+	public function get_news($slug = FALSE)
+	{
+		if ($slug === FALSE)
+		{
+			$query = $this->db->get('news');
+			return $query->result_array();
+		}
+		
+		$query = $this->db->get_where('news', array('slug' => $slug));
+		return $query->row_array();
+	}
 }
 ?>
